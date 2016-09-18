fbc5162f2107e231bf4ffb677a34ea571dcc8aaf
meeksfred <maxfred0025@gmail.com>
2016-09-18 15:46:03 -0700

    added eventemitters to run each transform

--- bitmap-team-awesome/index.js
+++ bitmap-team-awesome/index.js
@@ -3,15 +3,43 @@
 const readBitFile = require('./lib/bit-file-helper');
 const bitMapObject = require('./model/bit-map-constructor');
 const colorConstructor = require('./model/color-constructor');
+const EE = require('events');
+const ee = new EE();
 
 
-readBitFile(function(bitmap){
-  // console.log('work', bitmap);
-  // bitMapConstructor(bitmap);
-  // colorConstructor.invertColor(bitmap, bitMapObject);
-  colorConstructor.grayScaleColor(bitmap, bitMapObject);
-  // colorConstructor.rgbScaleColor(bitmap, bitMapObject);
-  // colorConstructor.changeColorBlue(bitmap, bitMapObject);
-  // colorConstructor.changeColorGreen(bitmap, bitMapObject);
-  // colorConstructor.changeColorRed(bitmap, bitMapObject);
+// readBitFile(function(bitmap, bitMapObject){
+//   // console.log('work', bitmap);
+//   // bitMapConstructor(bitmap);
+//   // colorConstructor.invertColor(bitmap, bitMapObject);
+//   console.log (bitmap, bitMapObject);
+//   // colorConstructor.grayScaleColor(bitmap, bitMapObject);
+//   // colorConstructor.rgbScaleColor(bitmap, bitMapObject);
+//   // colorConstructor.changeColorBlue(bitmap, bitMapObject);
+//   // colorConstructor.changeColorGreen(bitmap, bitMapObject);
+//   // colorConstructor.changeColorRed(bitmap, bitMapObject);
+// });
+
+ee.on('first', function(){
+  readBitFile(function(bitmap, bitMapObject){
+    // console.log (bitmap, bitMapObject);
+    colorConstructor.grayScaleColor(bitmap, bitMapObject);
+    ee.emit('second');
+  });
 });
+
+ee.on('second', function(){
+  readBitFile(function(bitmap, bitMapObject){
+    // console.log (bitmap, bitMapObject);
+    colorConstructor.rgbScaleColor(bitmap, bitMapObject);
+    ee.emit('third');
+  });
+});
+
+ee.on('third', function(){
+  readBitFile(function(bitmap, bitMapObject){
+    // console.log (bitmap, bitMapObject);
+    colorConstructor.invertColor(bitmap, bitMapObject);
+  });
+});
+
+ee.emit('first');
--- bitmap-team-awesome/model/bit-map-constructor.js
+++ bitmap-team-awesome/model/bit-map-constructor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function Bitmap(buffer){
+  Bitmap.string = buffer.toString('utf8', 0, 2);
   Bitmap.fullSize = buffer.readUInt32LE(2);
   Bitmap.colorOffSet = buffer.readUInt32LE(10);
   // console.log('constructor', buffer);
