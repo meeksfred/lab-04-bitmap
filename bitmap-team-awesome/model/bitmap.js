b95e9f9deed4c50715810ba92c049dac3f8293a0
meeksfred <maxfred0025@gmail.com>
2016-09-19 09:49:21 -0700

    remove console log

--- bitmap-team-awesome/model/bitmap.js
+++ bitmap-team-awesome/model/bitmap.js
@@ -4,6 +4,5 @@ module.exports = function Bitmap(buffer){
   Bitmap.string = buffer.toString('utf8', 0, 2);
   Bitmap.fullSize = buffer.readUInt32LE(2);
   Bitmap.colorOffSet = buffer.readUInt32LE(10);
-  // console.log('constructor', buffer);
   return Bitmap;
 };
